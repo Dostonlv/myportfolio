7dc972a9f50133ba521d583df51a4fa3aa9b1677
Itachi <Iamitachiuchixa@gmail.com>
2022-03-10 11:28:57 +0500

    redeploy2

--- pages/pages/Home/index.tsx
+++ pages/pages/Home/index.tsx
@@ -1,9 +1,13 @@
 import Particle from "../../particles";
 import HomeWr from "./HomeWrapper";
 import * as React from 'react';
+import{ReactNode} from 'react';
 
+type Props = {
+  children?: ReactNode;
+};``
 // @ts-ignore
-const Home = ({ children }:Props) => {
+const Home = ({ children }) => {
   return (
     <HomeWr>
       <Particle />
