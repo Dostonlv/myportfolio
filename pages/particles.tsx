3aa1450f01933d359a4cfa1be694294b19346c20
Itachi <Iamitachiuchixa@gmail.com>
2022-03-10 11:59:45 +0500

    redeploy4

--- pages/particles.tsx
+++ pages/particles.tsx
@@ -2,9 +2,26 @@ import React from "react";
 import particles_conf from "./particles.config";
 import Particles from "react-tsparticles";
 const Particle = () => {
-  return (
+    return (
     <div>
-      <Particles params={particles_conf} />
+      <Particles params={{
+          particles: {
+              color: {
+                  value: "#000000"
+              },
+              line_linked: {
+                  color: {
+                      value: "#000000"
+                  }
+              },
+              number: {
+                  value: 50
+              },
+              size: {
+                  value: 3
+              }
+          }
+      }} />
     </div>
   );
 };
